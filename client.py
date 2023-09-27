a2fea32214e5a7f2d0ea61d279520a924dae840b
鄭廷瑋 <zhengtingwei@zhengtingweis-MacBook-Air.local>
2023-09-27 12:03:20 +0800

    Add SIGINT handler.

--- client.py
+++ client.py
@@ -1,29 +1,42 @@
 import socket
 import sys
 import threading
+import signal 
 
 HOST, PORT = "0.0.0.0", 9999
 data = " ".join(sys.argv[1:])
-
-def handle_ui(sock):
-    while True:
-        received = str(sock.recv(1024), "utf-8")
-        print(received)
+def sigint_handler(signal, frame):
+    print("End")
+    # _sock.sendall(bytes("bye", "utf-8"))
+    _sock.close()
+    sys.exit(0)
+def handle_ui():
+    try:
+        while True:
+            received = str(_sock.recv(1024), "utf-8")
+            print(received)
+    except OSError:
+        pass
+    finally:
+        pass
 # Create a socket (SOCK_STREAM means a TCP socket)
+_sock = None
+signal.signal(signal.SIGINT, sigint_handler)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     # Connect to server and send data
     sock.connect((HOST, PORT))
     #sock.sendall(bytes(data + "\n", "utf-8"))
     #name = str(sock.recv(1024), "utf-8")
     name = "1"
-    thread = threading.Thread(target=handle_ui, args=(sock,))
+    _sock = sock
+    thread = threading.Thread(target=handle_ui)
     thread.start()
     while True:
         # Receive data from the server and shut down
         data = input(">")
-        sock.sendall(bytes(name+" "+data, "utf-8"))
+        _sock.sendall(bytes(name+" "+data, "utf-8"))
         print(f"Client {name}")
         print("Sent:     {}".format(data))
         if "bye" in data:
             break
-    socket.close()
+    sock.close()
--- server.py
+++ server.py
@@ -1,29 +1,49 @@
 
 import socket
 import threading
-
+import sys
+import signal
+import time
 SERVER, PORT= "0.0.0.0", 9999
 ADDR = (SERVER, PORT)
 FORMAT = 'UTF-8'
 
+class Event:
+    def __init__(self):
+        self.status = False
+exit_event = Event()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+def sigint_handler(signal,frame):
+    print("End")
+    exit_event.status = True
+    sys.exit(0)
 
 def handle_read(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
-    while True:
-        msg = conn.recv(1024).decode(FORMAT)
+    counter = 0
+    while not exit_event.status:
+        msg = conn.recv(1024).decode(FORMAT) 
+        if counter >= 10:
+            break
+        if len(msg) == 0:
+            counter += 1
+            continue
         print(msg)
+        import os
+        os.system(f"echo {msg} >> tmp.txt")
+        counter = 0
         if "bye" in msg:
             break
+    print("hanld_read done.")
     conn.close()
 
 # server -> client
 # [#num] string_to_send
 def handle_write():
-    while True:
+    while not exit_event.status:
         msg = input("Server > ")
         num, *_ = msg.split(" ")
         num = int(num)
@@ -41,7 +61,7 @@ def start():
     count = 0
     thread_write = threading.Thread(target=handle_write)
     thread_write.start()
-    while True:
+    while not exit_event:
         conn, addr = server.accept()
         count += 1
         dic[count] = conn
@@ -49,6 +69,6 @@ def start():
         thread_read.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
-
+signal.signal(signal.SIGINT, sigint_handler)
 print("[STARTING] server is starting...")
 start()
